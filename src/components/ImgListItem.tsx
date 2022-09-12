9d12527173f0c5f777719e0503e54dd23459ab42
exe.cute <90452860+Max211922@users.noreply.github.com>
2022-09-12 18:42:00 +0300

    Update ImgListItem.tsx

--- src/components/ImgListItem.tsx
+++ src/components/ImgListItem.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { DEFAULT_DESCRIPTION } from '../utils/constants';
-import { IconButton, ImageListItem, ImageListItemBar } from '@mui/material';
+import { CardActionArea, IconButton, ImageListItem, ImageListItemBar } from '@mui/material';
 import InfoIcon from '@mui/icons-material/Info';
 import { Photo } from '../models/Photo';
 
-const ImgListItem = ({photo, onClickHandler, styles}: {photo: Photo, onClickHandler?: (photo: Photo)=>void, styles?: React.CSSProperties}) => {
-    if (!photo) return <div>Loading...</div>
+const ImgListItem = ({photo, onClickHandler, styles}:
+                         { photo: Photo, onClickHandler?: (photo: Photo) => void, styles?: React.CSSProperties }) => {
     const description = photo.description || photo.alt_description || DEFAULT_DESCRIPTION;
 
     return (
         <ImageListItem key = {photo.id} cols = {1}>
-            <img style={styles}
-                 src = {`${photo.urls.regular}?w=248&fit=crop&auto=format`}
-                 width='100%'
-                 srcSet = {`${photo.urls.regular}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                 alt = {description}
-                 loading = "lazy"
-                 onClick={()=>onClickHandler ? onClickHandler(photo) : null}
-            />
+            <CardActionArea>
+                <img style = {styles}
+                     src = {`${photo.urls.regular}?w=248&fit=crop&auto=format`}
+                     width = '100%'
+                     srcSet = {`${photo.urls.regular}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                     alt = {description}
+                     loading = "lazy"
+                     onClick = {() => onClickHandler ? onClickHandler(photo) : null}
+                />
+            </CardActionArea>
             <ImageListItemBar
                 title = {description}
                 subtitle = {photo.user.username}
@@ -26,7 +28,7 @@ const ImgListItem = ({photo, onClickHandler, styles}: {photo: Photo, onClickHand
                         sx = {{color: 'rgba(255, 255, 255, 0.54)'}}
                         aria-label = {`info about ${photo.user.username}`}
                     >
-                        <InfoIcon color='secondary'/>
+                        <InfoIcon color = 'secondary'/>
                     </IconButton>
                 }
             />
